47d90d13db43612a22d0b7aa8ab4edb56905e3ac
andrewlfauth <andrewlfauth@gmail.com>
2022-09-21 14:21:24 -0700

    return error if no data

--- src/pages/api/posts.js
+++ src/pages/api/posts.js
@@ -6,7 +6,12 @@ export async function post({request}) {
     getPageData(pages[0]),
     getPageData(pages[1]),
   ])
+  
   let merged = [].concat.apply([], data)
 
+  if (!merged.length) {
+    return new Response(JSON.stringify({error: "No more data"}))
+  }
+  
   return new Response(JSON.stringify(merged))
 }
