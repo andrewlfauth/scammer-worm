481dbeda183be5b768f3fc34eba3508bcacfaad4
andrewlfauth <andrewlfauth@gmail.com>
2022-09-21 13:18:11 -0700

    fetch two pages at a time

--- src/pages/api/posts.js
+++ src/pages/api/posts.js
@@ -2,11 +2,11 @@ import getPageData from '../../utils/getPageData'
 
 export async function post({request}) {
   const {pages} = await request.json()
-  const data = await Promise.all([
+  let data = await Promise.all([
     getPageData(pages[0]),
     getPageData(pages[1]),
-  ]).then()
-  const merged = [].concat.apply([], data)
+  ])
+  let merged = [].concat.apply([], data)
 
-  return new Response(JSON.stringify({pages: merged}))
+  return new Response(JSON.stringify({d: 0}))
 }
